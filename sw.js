b92f1c39bf3f38559348f51a4e0a75ca973354c0
sanae <2337035050@qq.com>
2026-02-01 17:18:15 +0800

    Site updated: 2026-02-01 17:18:14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1768802343189";const preCache = ["/images/taichi.png","/images/bg1.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1769937493490";const preCache = ["/images/taichi.png","/images/bg1.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
